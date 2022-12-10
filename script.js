cdd2bb7be1e65065ac21fba53ff7c5df3bb13ec1
GoRbYsKkA <gleb.kovalchuk0609@gmail.com>
2022-12-10 20:17:35 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const firstNumber = Number(prompt('Введите первое число'));
+const secondNumber = Number(prompt('Введите второе число'));
+const thirdNumber = Number(prompt('Введите третье число'));
+
+const result = (firstNumber + secondNumber + thirdNumber) / 3;
+alert(result);
